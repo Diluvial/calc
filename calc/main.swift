7d7da52eab736f0297755725e827413125334f82
Trent Diamond <trent_diamond@me.com>
2019-04-06 14:39:21 +1100

    Almost fininshed

--- calc/main.swift
+++ calc/main.swift
@@ -8,7 +8,156 @@
 
 import Foundation
 
-var args = ProcessInfo.processInfo.arguments
-args.removeFirst() // remove the name of the program
+func checkEquation(_ userInput: [String]) {
+    var position: Int = 0
+    
+    //Will check if the input can be calculated
+    if userInput.count <= 0 {
+        //print("There are no values in your input")
+        exit(0)
+    }
+    
+    //Begins or ends with an integer
+    if userInput[0] == "/" || userInput[0] == "x" || userInput[0] == "%" || userInput[0] == "+" || userInput[0] == "-" {
+        //print("You must begin the equation with an integer")
+        exit(0)
+    }
+    if userInput[userInput.count - 1] == "/" || userInput[userInput.count - 1] == "x" || userInput[userInput.count - 1] == "%" || userInput[userInput.count - 1] == "+" || userInput[userInput.count - 1] == "-" {
+        //print("You must end the equation with an integer")
+        exit(0)
+    }
+    
+    //Check the Integers entered are legit
+    for value in userInput {
+        if value != "/" && value != "x" && value != "%" && value != "+" && value != "-" {
+            if let _ = Int(value) {
+                position += 1
+            } else {
+                //print("You must enter an integer")
+                exit(0)
+            }
+        }
+    }
+    
+    //Will check to see if there are two equations running consectutive to each other
+    position = 0
+    for value in userInput {
+        if value == "/" || value == "x" || value == "%" || value == "+" || value == "-" {
+            if userInput[position + 1] == "/" || userInput[position + 1] == "x" || userInput[position + 1] == "%" || userInput[position + 1] == "+" || userInput[position + 1] == "-"{
+                //print("You cannot have two equations running consectutive to each other")
+                exit(0)
+            }
+            position += 1
+        } else {
+            position += 1
+        }
+    }
+}
 
-print(Int(args[0])!)
+//Will solve the equation if passed checkString
+func solve(_ equation: [String]) -> Int {
+    var equationCopy = equation
+    //dump(equationCopy)
+    var position: Int = 0
+    
+    // Check for '/' operands
+    for value in equationCopy {
+        if value == "/" {
+
+            //Will check if being devided by zero
+            if equationCopy[position + 1] == "0" {
+                exit(0)
+            } else if equationCopy[position - 1] == "0" {
+                equationCopy[position + 1] = "0"
+                
+            //Will run the normal operation
+            } else {
+                equationCopy[position + 1] = String(Int(equationCopy[position - 1])! / Int(equationCopy[position + 1])!)
+            }
+            
+            //Remove values from string
+            equationCopy.remove(at: position)
+            equationCopy.remove(at: position - 1)
+            position -= 1
+            //dump(equationCopy)
+        } else {
+            position += 1
+        }
+    }
+    
+    position = 0
+    // Check for 'x' operands
+    for value in equationCopy {
+        if value == "x" {
+
+            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! * Int(equationCopy[position + 1])!)
+            
+            // Remove values from string
+            equationCopy.remove(at: position)
+            equationCopy.remove(at: position - 1)
+            position -= 1
+            //dump(equationCopy)
+        } else {
+            position += 1
+        }
+    }
+    
+    position = 0
+    // Check for '%' operands
+    for value in equationCopy {
+        if value == "%" {
+
+            //Will check if being mod by zero
+            if equationCopy[position + 1] == "0" {
+                exit(0)
+            } else if equationCopy[position - 1] == "0" {
+                equationCopy[position + 1] = "0"
+                
+            //Will run the normal operation
+            } else {
+            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! % Int(equationCopy[position + 1])!)
+            }
+            
+            // Remove values from string
+            equationCopy.remove(at: position)
+            equationCopy.remove(at: position - 1)
+            position -= 1
+
+        } else {
+            position += 1
+        }
+    }
+    
+    position = 0
+    // Check for '+' & '-' operands
+    for value in equationCopy {
+        if value == "+" {
+
+            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! + Int(equationCopy[position + 1])!)
+            
+            // Remove values from string
+            equationCopy.remove(at: position)
+            equationCopy.remove(at: position - 1)
+            position -= 1
+
+        } else if value == "-" {
+            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! - Int(equationCopy[position + 1])!)
+
+            // Remove values from string
+            equationCopy.remove(at: position)
+            equationCopy.remove(at: position - 1)
+            position -= 1
+
+        } else {
+            position += 1
+        }
+    }
+    
+    let finalValue = Int(equationCopy[0])!
+    return finalValue
+}
+
+var userInput = ProcessInfo.processInfo.arguments
+userInput.removeFirst() // remove the name of the program
+checkEquation(userInput)
+print(solve(userInput))
