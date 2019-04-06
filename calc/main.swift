9ea1c4c380f0eb4a36d1975aa966d5f6eabc6f47
Trent Diamond <trent_diamond@me.com>
2019-04-06 23:35:12 +1100

    Final project

--- calc/main.swift
+++ calc/main.swift
@@ -8,161 +8,166 @@
 
 import Foundation
 
-func checkEquation(_ userInput: [String]) {
-    var position: Int = 0
+class mathEquation {
+    var equation: [String]
     
-    //Will check if the input can be calculated
-    if userInput.count <= 0 {
-        //print("There are no values in your input")
-        exit(1)
+    init(equation: [String]) {
+        self.equation = equation
     }
     
-    //Begins or ends with an integer
-    if userInput[0] == "/" || userInput[0] == "x" || userInput[0] == "%" || userInput[0] == "+" || userInput[0] == "-" {
-        //print("You must begin the equation with an integer")
-        exit(1)
-    }
-    if userInput[userInput.count - 1] == "/" || userInput[userInput.count - 1] == "x" || userInput[userInput.count - 1] == "%" || userInput[userInput.count - 1] == "+" || userInput[userInput.count - 1] == "-" {
-        //print("You must end the equation with an integer")
-        exit(1)
-    }
-    
-    //Check the Integers entered are legit
-    for value in userInput {
-        if value != "/" && value != "x" && value != "%" && value != "+" && value != "-" {
-            if let _ = Int(value) {
+    func checkEquation() {
+        var position: Int = 0
+        
+        //Will check if the input can be calculated
+        if equation.count <= 0 {
+            print("There are no values in your input")
+            exit(1)
+        }
+        
+        //Begins or ends with an integer
+        if equation[0] == "/" || equation[0] == "x" || equation[0] == "%" || equation[0] == "+" || equation[0] == "-" {
+            print("You must begin the equation with an integer")
+            exit(1)
+        }
+        if equation[equation.count - 1] == "/" || equation[equation.count - 1] == "x" || equation[equation.count - 1] == "%" || equation[equation.count - 1] == "+" || equation[equation.count - 1] == "-" {
+            print("You must end the equation with an integer")
+            exit(1)
+        }
+        
+        //Check the Integers entered are legit
+        for value in equation {
+            if value != "/" && value != "x" && value != "%" && value != "+" && value != "-" {
+                if let _ = Int(value) {
+                    position += 1
+                } else {
+                    print("You must enter an integer")
+                    exit(1)
+                }
+            }
+        }
+        
+        //Will check to see if there are two equations running consectutive to each other
+        position = 0
+        for value in equation {
+            if value == "/" || value == "x" || value == "%" || value == "+" || value == "-" {
+                if equation[position + 1] == "/" || equation[position + 1] == "x" || equation[position + 1] == "%" || equation[position + 1] == "+" || equation[position + 1] == "-"{
+                    print("You cannot have two equations running consectutive to each other")
+                    exit(1)
+                }
                 position += 1
             } else {
-                //print("You must enter an integer")
-                exit(1)
+                position += 1
             }
         }
     }
     
-    //Will check to see if there are two equations running consectutive to each other
-    position = 0
-    for value in userInput {
-        if value == "/" || value == "x" || value == "%" || value == "+" || value == "-" {
-            if userInput[position + 1] == "/" || userInput[position + 1] == "x" || userInput[position + 1] == "%" || userInput[position + 1] == "+" || userInput[position + 1] == "-"{
-                //print("You cannot have two equations running consectutive to each other")
-                exit(1)
-            }
-            position += 1
-        } else {
-            position += 1
+    func calculate() {
+        var position: Int = 0
+        
+        func removePositions() {
+            equation.remove(at: position)
+            equation.remove(at: position - 1)
+            position -= 1
         }
-    }
-}
-
-//Will solve the equation if passed checkString
-func solve(_ equation: [String]) -> Int {
-    var equationCopy = equation
-    //dump(equationCopy)
-    var position: Int = 0
-    
-    // Check for '/' operands
-    for value in equationCopy {
-        if value == "/" {
-
-            //Will check if being devided by zero
-            if equationCopy[position + 1] == "0" {
-                exit(1)
-            } else if equationCopy[position - 1] == "0" {
-                equationCopy[position + 1] = "0"
+        
+        // Check for '/' operands
+        for value in equation {
+            if value == "/" {
                 
-            //Will run the normal operation
-            } else {
-                equationCopy[position + 1] = String(Int(equationCopy[position - 1])! / Int(equationCopy[position + 1])!)
-            }
-            
-            //Remove values from string
-            equationCopy.remove(at: position)
-            equationCopy.remove(at: position - 1)
-            position -= 1
-            //dump(equationCopy)
-        } else {
-            position += 1
-        }
-    }
-    
-    position = 0
-    // Check for 'x' operands
-    for value in equationCopy {
-        if value == "x" {
-
-            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! * Int(equationCopy[position + 1])!)
-            
-            // Remove values from string
-            equationCopy.remove(at: position)
-            equationCopy.remove(at: position - 1)
-            position -= 1
-            
-        } else {
-            position += 1
-        }
-    }
-    
-    position = 0
-    // Check for '%' operands
-    for value in equationCopy {
-        if value == "%" {
-
-            //Will check if being mod by zero
-            if equationCopy[position + 1] == "0" {
-                exit(1)
-            } else if equationCopy[position - 1] == "0" {
-                equationCopy[position + 1] = "0"
+                //Will check if being devided by zero
+                if equation[position + 1] == "0" {
+                    print("You cannot devide by zero")
+                    exit(1)
+                } else if equation[position - 1] == "0" {
+                    equation[position + 1] = "0"
+                    
+                    //Will run the normal operation
+                } else {
+                    equation[position + 1] = String(Int(equation[position - 1])! / Int(equation[position + 1])!)
+                }
                 
-            //Will run the normal operation
+                //Remove values from string
+                removePositions()
+                
             } else {
-            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! % Int(equationCopy[position + 1])!)
+                position += 1
             }
-            
-            // Remove values from string
-            equationCopy.remove(at: position)
-            equationCopy.remove(at: position - 1)
-            position -= 1
-
-        } else {
-            position += 1
         }
-    }
-    
-    position = 0
-    // Check for '+' & '-' operands
-    for value in equationCopy {
-        if value == "+" {
-            
-            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! + Int(equationCopy[position + 1])!)
-
-            // Remove values from string
-            equationCopy.remove(at: position)
-            equationCopy.remove(at: position - 1)
-            position -= 1
-
-        } else if value == "-" {
-            equationCopy[position + 1] = String(Int(equationCopy[position - 1])! - Int(equationCopy[position + 1])!)
-
-            // Remove values from string
-            equationCopy.remove(at: position)
-            equationCopy.remove(at: position - 1)
-            position -= 1
-
-        } else {
-            position += 1
+        
+        position = 0
+        // Check for 'x' operands
+        for value in equation {
+            if value == "x" {
+                
+                equation[position + 1] = String(Int(equation[position - 1])! * Int(equation[position + 1])!)
+                
+                // Remove values from string
+                removePositions()
+                
+            } else {
+                position += 1
+            }
         }
+        
+        position = 0
+        // Check for '%' operands
+        for value in equation {
+            if value == "%" {
+                
+                //Will check if being mod by zero
+                if equation[position + 1] == "0" {
+                    print("You cannot mod by zero")
+                    exit(1)
+                } else if equation[position - 1] == "0" {
+                    equation[position + 1] = "0"
+                    
+                    //Will run the normal operation
+                } else {
+                    equation[position + 1] = String(Int(equation[position - 1])! % Int(equation[position + 1])!)
+                }
+                
+                // Remove values from string
+                removePositions()
+                
+            } else {
+                position += 1
+            }
+        }
+        
+        position = 0
+        // Check for '+' & '-' operands
+        for value in equation {
+            if value == "+" {
+                
+                equation[position + 1] = String(Int(equation[position - 1])! + Int(equation[position + 1])!)
+                
+                // Remove values from string
+                removePositions()
+                
+            } else if value == "-" {
+                equation[position + 1] = String(Int(equation[position - 1])! - Int(equation[position + 1])!)
+                
+                // Remove values from string
+                removePositions()
+                
+            } else {
+                position += 1
+            }
+        }
+        
+        //Will check if there are more than one integer remaining
+        if equation.count > 1 {
+            print("Calcualtion cannot be complete, as missing an equation between two numbers")
+            exit(1)
+        }
+        
+        print(Int(equation[0])!)
     }
-    
-    //Will check if there are more than one integer remaining
-    if equationCopy.count > 1 {
-        exit(1)
-    }
-    
-    let finalValue = Int(equationCopy[0])!
-    return finalValue
 }
 
 var userInput = ProcessInfo.processInfo.arguments
 userInput.removeFirst() // remove the name of the program
-checkEquation(userInput)
-print(solve(userInput))
+
+let equation = mathEquation(equation: userInput)
+equation.checkEquation()
+equation.calculate()
